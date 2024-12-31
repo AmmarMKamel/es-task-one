d16ec7177887882d1d46983ceb3a8adc44caba3f
AmmarMKamel <ammarkamel9899@gmail.com>
2024-12-31 12:07:58 +0200

    Finish the main task

--- DayOneTask/Program.cs
+++ DayOneTask/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        const int smallCarpetPrice = 25;
+        const int largeCarpetPrice = 35;
+        const double taxRate = 0.06;
+        const int validEstimateDurationInDays = 30;
+
+        Console.WriteLine("Number of small carpets:");
+        int smallCarpetsCount = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Number of large carpets:");
+        int largeCarpetsCount = Convert.ToInt32(Console.ReadLine());
+
+        double smallCarpetsTotal = smallCarpetPrice * smallCarpetsCount;
+        double largeCarpetsTotal = largeCarpetPrice * largeCarpetsCount;
+        double totalWithoutTaxes = smallCarpetsTotal + largeCarpetsTotal;
+        double taxValue = totalWithoutTaxes * taxRate;
+        double totalAfterTaxes = totalWithoutTaxes + taxValue;
+
+        Console.WriteLine($"Price per small carpet: ${smallCarpetPrice}");
+        Console.WriteLine($"Price per large carpet: ${largeCarpetPrice}");
+        Console.WriteLine($"Cost: ${totalWithoutTaxes}");
+        Console.WriteLine($"Tax: ${taxValue}");
+        Console.WriteLine("=========================================");
+        Console.WriteLine($"Total estimate: ${totalAfterTaxes}");
+        Console.WriteLine($"This estimate is valid for {validEstimateDurationInDays} days");
     }
 }
